b9d3dd551d6844ef1371f415fb457e68d88847b0
Michael Schroeder <schroeder@princeton.edu>
2024-04-10 09:45:56 -0400

    reading analytical data into python working

--- Numerical_Methods/run_cases.py
+++ Numerical_Methods/run_cases.py
@@ -1,4 +1,8 @@
 from fwd_euler import fwd_euler
+from getting_analytical_data import get_anal_data
+
+# getting analytical data
+N,times,rs_linspace,thetas_linspace,rs,thetas,Z = get_anal_data()
 
 
 # Constant Values
@@ -6,11 +10,11 @@ R = 1.0
 A = 1.0
 omega = 1.0
 c = 1.0
-tmax = 1
+tmax = times[-1]
 
 #create grid size
-num_r = 100  # Number of radial grid points
-num_theta = 100  # Number of angular grid points
-num_t = 1000  # Number of time steps
+num_r = N # Number of radial grid points
+num_theta = N  # Number of angular grid points
+num_t = len(times)  # Number of time steps
 
 fwd_euler(R, A, omega, c, tmax, num_t, num_r, num_theta)
